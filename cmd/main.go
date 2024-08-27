7b91ed8960ee04516aa357d188017fcde0ac78da
aosindo <andyovvo8@gmail.com>
2024-08-27 19:07:08 +0300

    added main

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"groupie/internals/routes"
+	"net/http"
+
+	"groupie/utils"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	routes.RegisterRoutes(mux)
+
+	wrappedMux := routes.RouteChecker(mux)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: wrappedMux,
+	}
+
+	fmt.Println("=====================================\nserver running @http://localhost:8080")
+	err := server.ListenAndServe()
+	if err != nil {
+		utils.ErrorHandler("web")
+	}
+}
+
